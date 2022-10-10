0fa417e276d1169e4e5b88d24add42108622d00f
Kristy Guo <krisxxg@gmail.com>
2022-10-10 16:47:25 -0700

    initial commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,15 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { withTRPC } from '@trpc/next'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
 
-export default MyApp
+
+export default withTRPC({
+  config({ctx}) {
+
+    const url = process.env.NEXT_PUBLIC_VERCEL_URL ? `http://$n`
+  }
+})
